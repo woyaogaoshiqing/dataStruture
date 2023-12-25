2e6ec29bf021ffc470424f6bdf5ec56c4f878bd3
woyaogaoshiqing <402824251@qq.com>
2023-12-25 15:50:43 +0800

    [修改]完善二叉搜索树的逻辑结构

--- BinarySearchTree/BinarySearchTree.c
+++ BinarySearchTree/BinarySearchTree.c
@@ -21,6 +21,7 @@ int BinarySearchTreeInit(BinarySearchTree **pBstree)
     }
     
     memset(bstree, 0, sizeof(BinarySearchTree) * 1);
+    /*初始化树*/
     {
     bstree->root = NULL;
     bstree->size = 0;
@@ -43,7 +44,47 @@ int BinarySearchTreeInit(BinarySearchTree **pBstree)
 }
 
 /*二叉搜索树的插入*/
-int BinarySearchTreeInser(BinarySearchTree **pBstree, ELEMENTTYPE val)
+int BinarySearchTreeInser(BinarySearchTree *pBstree, ELEMENTTYPE val)
 {
+    int ret = 0;
+    if (pBstree->size == 0)
+    {
+        /*更新树节点*/
+        (pBstree->size)++;
+        pBstree->root->data = val;
+        return ret;
+    }
+    /*指向根节点*/
+    BSTreeNode * traveNode = pBstree->root;
+    BSTreeNode * parentNode = pBstree->root;
+    int cmp = 0;
+    while (traveNode->data != NULL)
+    {
+        /*标记父节点*/
+        parentNode = traveNode;
+        cmp = val - traveNode->data;
+        if (cmp < 0)
+        {
+            traveNode = traveNode->left;
+        }
+        if (cmp > 0)
+        {
+            traveNode = traveNode->right;
+        }
+        else
+        {
+            /*插入元素 = 遍历到的节点*/
+            return ret;
+        }
+    }
+    if (cmp < 0)
+    {
+        parentNode->left = ;
+    }
+    else
+    {
+        parentNode->right = ;
+    }
 
+    return ret;
 }
